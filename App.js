cd560b25cbd4086ae532d3648dd6265d8284990d
amir-doukh <amir-doukh@protonmail.com>
2019-02-27 18:21:03 +0100

    camera

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View,TextInput ,Button,StyleSheet,FlatList,Vibration} from 'react-native';
+import { View,TextInput ,Button,StyleSheet,FlatList,Vibration,CameraRoll,ScrollView} from 'react-native';
 import films from './helpers/filmsData';
 import FilmItems from './componants/filmItems';
 
@@ -12,11 +12,24 @@ export default class App extends React.Component {
   StartVibration(){
 Vibration.vibrate(PATTERN);
   }
+  _handleButtonPress = () => {
+    CameraRoll.getPhotos({
+        first: 20,
+        assetType: 'Photos',
+      })
+      .then(r => {
+        this.setState({ photos: r.edges });
+      })
+      .catch((err) => {
+         //Error Loading Images
+         console.log("erreur");
+      });
+    };
   render() {
     return (
       <View style={styles.main_container}>
         <TextInput style={StyleSheet.textinput} placeholder="entrer votre nom" ></TextInput>
-        <Button title="recherche" onPress={this.StartVibration}> </Button>
+        <Button title="recherche" onPress={this._handleButtonPress}> </Button>
         <FlatList
 
   data={films}
@@ -26,6 +39,22 @@ Vibration.vibrate(PATTERN);
   renderItem={({item}) => <FilmItems film={item}/>}
 
 />
+<View>
+<ScrollView>
+       {this.state.photos.map((p, i) => {
+       return (
+         <Image
+           key={i}
+           style={{
+             width: 300,
+             height: 100,
+           }}
+           source={{ uri: p.node.image.uri }}
+         />
+       );
+     })}
+     </ScrollView>
+</View>
       </View>
     )
   }
